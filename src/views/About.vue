cf82bd93fa3dc6ea1204b770d9e366346ae1dd54
EJ Taylan <emmanuel_john_taylan@dlsu.edu.ph>
2026-01-16 19:09:11 +0800

    yes

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,13 +4,13 @@
 
     <div class="paragraph">
       <div>
-        I'm <strong>EJ Taylan</strong>, a game developer and college undergraduate based in Santa Rosa, Laguna. <br/>
-        Currently I'm pursuing a Bachelor of Science in Interactive Entertainment major in Game Development from DLSU-Laguna.
+        I'm <strong>EJ Taylan</strong>, a game developer and college undergrad based in Santa Rosa, Laguna. <br/>
+        Currently I'm pursuing a <strong> Bachelor of Science in Interactive Entertainment major in Game Development </strong> from DLSU-Laguna.
         I specialize in UI/UX and gameplay mechanics, with interest in narratives and level and sound design.
       </div>
 
-      <div style="margin-top: 20px;">Here are some <router-link to="/game-projects"> projects </router-link> I've worked on, either for school or as a personal project.
-        <router-link to="/other-projects">Here's</router-link> some of my skills and courses/certifications I've taken, and here is my <router-link to="/resume">resume</router-link>.</div>
+      <div style="margin-top: 20px;">Here are some <router-link to="/game-projects"> projects </router-link> I've worked on, either for school or as a personal project.   
+        Here's some of my <router-link to="/other-projects">skills</router-link> and courses/certifications I've taken, and here is my <router-link to="/resume">resume</router-link>.</div>
 
       <div style="margin-top: 40px;">I'm <strong>currently looking for a job</strong> as a game developer. You can reach me at <a href="mailto:ejrtaylan@gmail.com">ejrtaylan@gmail.com</a> or <router-link to="/contact">through here</router-link>.</div>
     </div>
